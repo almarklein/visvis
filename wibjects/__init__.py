6a573c1522578c84458a7fd6264e6f15b6521c59
almar <almar@localhost>
2011-03-07 14:59:45 +0100

    better description of App

---
+++ wibjects/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010, Almar Klein
+#
+# Visvis is distributed under the terms of the (new) BSD License.
+# The full license can be found in 'license.txt'.
+
+""" Module wibjects
+
+All wibjects are inserted in this namespace, thereby providing
+the user with a list of all wibjects. All wibjects are also
+inserted in the root visvis namespace.
+
+"""
+
+from visvis.core import (  Wibject, BaseFigure, AxesContainer, Axes, 
+                           Box, DraggableBox, Label, Legend,
+                        )
+
+from visvis.wibjects.buttons import PushButton, ToggleButton, RadioButton
+from visvis.wibjects.sliders import BaseSlider, Slider, RangeSlider
+from visvis.wibjects.colorWibjects import Colorbar, ColormapEditor, ClimEditor
+from visvis.wibjects.title import Title
