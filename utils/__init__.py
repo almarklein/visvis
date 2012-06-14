9fe913de27c883f6aa41b646e0563e9292a85d1e
almar <almar@localhost>
2012-06-14 13:13:17 +0200

    Adding graph.py as a utility module.

---
+++ utils/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2012, Almar Klein
+#
+# Visvis is distributed under the terms of the (new) BSD License.
+# The full license can be found in 'license.txt'.
+
+""" Package visvis.utils
+
+This package contains a few utilities that are not a core part of visvis.
+Some utilities are used inside visvis (such as ssdf), others are purely
+stand alone modules that have some relation to visualization.
+
+The intended way to import utilities is: "from visvis.utils import utilname".
+
+The currrent utilities are:
+
+  * ssdf - simple structured data format.
+  * graph - Representing points/nodes that are connected by edges.
+
+"""
