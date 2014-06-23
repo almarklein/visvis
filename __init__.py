00237c50a06fe82e6130c403c2dcdbcc620c7286
Almar Klein <almar.klein@gmail.com>
2014-06-23 13:35:37 +0200

    version number to 1.9.1

--- __init__.py
+++ __init__.py
@@ -44,7 +44,7 @@ Visvis is maintained by Almar Klein.
 
 """
 
-__version__  = '1.9' 
+__version__  = '1.9.1' 
 
 # Loose sub-modules and sub-packages
 from visvis.utils.pypoints import Point, Pointset, Aarray, Quaternion
