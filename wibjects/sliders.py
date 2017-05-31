c461fc50c236d9020cef56d8655f0c0f416fbfe8
Almar Klein <almar.klein@gmail.com>
2017-06-01 00:05:30 +0200

    update some license texts

--- wibjects/sliders.py
+++ wibjects/sliders.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2012, Almar Klein
+# Copyright (C) 2012-2017, Almar Klein
 #
 # Visvis is distributed under the terms of the (new) BSD License.
 # The full license can be found in 'license.txt'.
