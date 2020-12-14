e1aff4d663f61b05e1021a67e325660680061e4b
Almar Klein <almar.klein@gmail.com>
2020-12-14 12:26:07 +0100

    bump version

--- __init__.py
+++ __init__.py
@@ -45,7 +45,7 @@ Visvis is maintained by Almar Klein.
 
 """
 
-__version__  = '1.12.4'
+__version__  = '1.13.0'
 
 # Loose sub-modules and sub-packages
 from visvis.utils.pypoints import Point, Pointset, Aarray, Quaternion
